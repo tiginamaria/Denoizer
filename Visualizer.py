b32e93bb8f826a73ba50e974e6126e2fcd40df5a
Maria Tigina <tigmah@mail.ru>
2019-11-05 17:29:12 +0300

    Fixed subtraction with adding 'modules' = max(0, amplitude after subtraction)

--- Visualizer.py
+++ Visualizer.py
@@ -1,18 +1,40 @@
 import librosa, librosa.display
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, IPython.display as ipd
+import numpy as np
+import seaborn as sns
+
+sns.set()
+
+def display_plots(title, x, amplitude_x, sr):
+    plt.figure()
+    ax1 = plt.subplot(2, 1, 1)
+    amplitude_spectrum_plot(title, amplitude_x, sr)
+    ax2 = plt.subplot(2, 1, 2)
+    wave_plot(title, x, sr)
+    plt.show()
+    spectrogam_plot(title, amplitude_x)
+    plt.show()
+
+def spectrogam_plot(title, x):
+    librosa.display.specshow(librosa.amplitude_to_db(x, ref = np.max), y_axis = 'log', x_axis = 'time')
+    plt.grid(True)
+    plt.title(title + ' power spectrogram')
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
 
 def wave_plot(title, x, sr):
-    plt.grid(True)
+    ipd.Audio(x, rate=sr)
     plt.title(title)
-    plt.xlabel('amplitude')
-    plt.ylabel('time (s)')
-    plt.figure(figsize=(14, 5))
-    librosa.display.waveplot(x, sr=sr)
-    plt.show()
+    plt.ylabel('Amplitude')
+    plt.xlabel('Time')
+    plt.grid(True)
+    time = np.arange(x.shape[0]) / sr
+    plt.plot(time, x)
+    # librosa.display.waveplot(x, sr=sr)
 
 def amplitude_spectrum_plot(title, x, sr):
+    x = np.mean(x, axis=1)
     plt.title(title + ' amplitude spectrum plot')
-    plt.ylabel('amplitude')
-    plt.xlabel('frequency')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency')
     plt.plot(x.T)
-    plt.show()
