e202e63dc44cf92146c96b55f62037059b45c31e
Maria Tigina <tigmah@mail.ru>
2019-11-27 23:41:50 +0300

    Added implementation of simple RNN to denoise waves

--- FilterUtils.py
+++ FilterUtils.py
@@ -1,9 +1,10 @@
 import librosa, librosa.display
 import numpy as np
+import scipy
 
 
-def import_signal(filename, norm=1):
-    """Imports a .wav file as a scaled float numpy array
+def read_signal(filename, norm=1):
+    """Read a .wav file as a scaled float numpy array
     :param filename: audio's filename
     :return signal: imported signal from .wav
     :return sr: sampling rate
@@ -14,6 +15,16 @@ def import_signal(filename, norm=1):
     return signal, sr, norm
 
 
+def write_signal(filename, signal, sr, norm=1):
+    """Write a signal to .wav
+    :param filename: audio's filename
+    :param signal: signal to write into file
+    :param sr: sampling rate
+    :param norm: normalization coefficient
+    """
+    scipy.io.wavfile.write(filename, sr, signal * norm)
+
+
 def decompose(fft):
     """Decomposes fft of signal to magnitude spectrum nd phase spectrum
     :param fft: fft of signal
@@ -21,4 +32,15 @@ def decompose(fft):
     :return phase: phase spectrum
     """
     return np.abs(fft), np.angle(fft)
-  
+
+
+def fit_size(signal, size):
+    """Generate signal of given size by concatenating given signal
+    :param signal: input signal
+    :param size: desired signal size
+    :return pumped_signal: pumped signal of given size
+    """
+    pumped_signal = signal
+    while len(pumped_signal) < size:
+        pumped_signal = np.concatenate((signal, signal))
+    return pumped_signal[:size]
